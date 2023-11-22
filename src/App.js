72217255b120cb647ed748deefbb20a7a9a93d12
Conivety <93612774+Conivety@users.noreply.github.com>
2023-11-22 16:41:41 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Weather from './weather';
+
+const App = () => {
+  return (
+    <div>
+      {/* ... other components */}
+      <Weather />
+    </div>
+  );
+};
+
+export default App;
